fd197c4fb9a47f3b47223c8eba3005b6bcf40625
Van Ortega <ortegavan@live.com>
2022-08-16 20:25:27 -0300

    [chore] commit inicial

---
+++ Teste/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+
+namespace Teste
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = HttpClientFactory.Create();
+            httpClient.DefaultRequestHeaders.Add("X-Vault-Token", "s.sZlgbICx7QCdfT87GZzE3VGU");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var url = "http://127.0.0.1:8200/v1/kv/my-secret";
+
+            var json = JObject.Parse(await httpClient.GetStringAsync(url));
+
+            Console.WriteLine(json);
+        }
+    }
+}
